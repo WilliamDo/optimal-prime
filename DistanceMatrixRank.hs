2584ff18a098f931a57747b06ff3f959a40445c5
William Do <william@ultimaspin.com>
2015-06-20 22:22:33 +0100

    formatting the code for better readability

--- DistanceMatrixRank.hs
+++ DistanceMatrixRank.hs
@@ -6,16 +6,21 @@ import Data.Matrix as M
 
 import Statistics.Sample
 
-data DistanceMatrix = DistanceMatrix { distanceMatrix :: Matrix Double, origins :: [String], destinations :: [String] }
-	deriving (Show)
+data DistanceMatrix = DistanceMatrix 
+									{ 
+										distanceMatrix :: Matrix Double, 
+										origins :: [String], 
+										destinations :: [String] 
+									}
+									deriving (Show)
 
 rankDestinations :: DistanceMatrix -> [(String, Double)]
 rankDestinations m = L.zip (destinations m) (L.map stdDev $ columns (distanceMatrix m))
 
 columns :: Matrix a -> [Vector a]
-columns m = L.map ((flip $ M.getCol) m) [1..x]
+columns m = L.map ((flip $ M.getCol) m) [1..cols]
 	where
-		x = M.ncols m
+		cols = M.ncols m
 
 testMatrix = DistanceMatrix {distanceMatrix = M.fromLists [[1, 2], [3, 100]], origins = ["A", "B"], destinations = ["C", "D"]}
 
