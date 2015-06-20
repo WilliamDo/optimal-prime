69fb15f2f233d4ba8487fe427d42bb4a553f3d80
William Do <william@ultimaspin.com>
2015-06-20 18:21:22 +0100

    add new core module for ranking distance matrix

---
+++ DistanceMatrixRank.hs
@@ -0,0 +1,21 @@
+module DistanceMatrixRank where
+
+import Data.List as L
+import Data.Vector as V
+import Data.Matrix as M
+
+import Statistics.Sample
+
+data DistanceMatrix = DistanceMatrix { distanceMatrix :: Matrix Double, origins :: [String], destinations :: [String] }
+	deriving (Show)
+
+rankDestinations :: DistanceMatrix -> [(String, Double)]
+rankDestinations m = L.zip (destinations m) (L.map stdDev $ columns (distanceMatrix m))
+
+columns :: Matrix a -> [Vector a]
+columns m = L.map ((flip $ M.getCol) m) [1..x]
+	where
+		x = M.ncols m
+
+testMatrix = DistanceMatrix {distanceMatrix = M.fromLists [[1, 2], [3, 100]], origins = ["A", "B"], destinations = ["C", "D"]}
+
