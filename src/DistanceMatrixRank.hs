6b20d8e6dbeaf2b75be860bcec7a907752e091fe
William Do <william@ultimaspin.com>
2017-01-08 22:28:57 +0000

    convert tabs to spaces

--- src/DistanceMatrixRank.hs
+++ src/DistanceMatrixRank.hs
@@ -7,27 +7,27 @@ import Data.Matrix as M
 import Statistics.Sample
 
 data DistanceMatrix = DistanceMatrix
-									{
-										distanceMatrix :: Matrix Double,
-										origins :: [String],
-										destinations :: [String]
-									}
-									deriving (Show)
+                      {
+                        distanceMatrix :: Matrix Double,
+                        origins :: [String],
+                        destinations :: [String]
+                      }
+                      deriving (Show)
 
 rankDestinations :: DistanceMatrix -> [(String, Double)]
 rankDestinations m = L.zip (destinations m) (L.map stdDev $ columns (distanceMatrix m))
 
 columns :: Matrix a -> [Vector a]
 columns m = L.map (flip M.getCol m) [1..cols]
-	where
-		cols = M.ncols m
+  where
+    cols = M.ncols m
 
 testMatrix = DistanceMatrix {distanceMatrix = M.fromLists [[1, 2], [3, 100]], origins = ["A", "B"], destinations = ["C", "D"]}
 
 foo :: IO (Int)
 foo = do
-	number <- fmap toString getLine
-	return (number)
+  number <- fmap toString getLine
+  return (number)
 
 toString :: String -> Int
 toString _ = 123
